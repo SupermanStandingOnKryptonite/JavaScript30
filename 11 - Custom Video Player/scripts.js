f2a996a422d149ba052a565be76d376a109a8b05
chtysl <devCihat@gmail.com>
2020-11-15 23:23:10 +0300

    fullscreen ability added

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,6 +6,7 @@ const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
+const screen = player.querySelector(".full");
 
 /* Build out functions */
 function togglePlay() {
@@ -36,6 +37,10 @@ function scrub(e) {
   video.currentTime = scrubTime;
 }
 
+function fullScreen() {
+  player.requestFullscreen();
+}
+
 /* Hook up the event listeners */
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
@@ -54,3 +59,4 @@ progress.addEventListener("click", scrub);
 progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
 progress.addEventListener("mousedown", () => (mousedown = true));
 progress.addEventListener("mouseup", () => (mousedown = false));
+screen.addEventListener("click", fullScreen);
